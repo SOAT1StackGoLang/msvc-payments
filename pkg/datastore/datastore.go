8b5149a0d35243f4946f6662d81f1484720946b8
Lucas Arruda <zatesec@loatecs.com>
2024-01-17 23:19:13 -0300

    Add Redis pub/sub methods to the RedisStore

--- pkg/datastore/datastore.go
+++ pkg/datastore/datastore.go
@@ -1,6 +1,8 @@
 package datastore
 
 import (
+	"time"
+
 	"github.com/go-redis/redis"
 )
 
@@ -21,4 +23,45 @@ func NewRedisStore(addr string, password string, db int) (*RedisStore, error) {
 	}
 
 	return &RedisStore{Client: client}, nil
+}
+
+// Set adds a key-value pair to the store
+func (s *RedisStore) Set(key string, value interface{}, expiration time.Duration) error {
+	err := s.Client.Set(key, value, expiration).Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Get retrieves a value from the store by its key
+func (s *RedisStore) Get(key string) (string, error) {
+	value, err := s.Client.Get(key).Result()
+	if err == redis.Nil {
+		return "", nil
+	} else if err != nil {
+		return "", err
+	}
+	return value, nil
+}
+
+// Implement your pub/sub methods here
+// Publish sends a message to a channel
+func (s *RedisStore) Publish(channel string, message interface{}) error {
+	err := s.Client.Publish(channel, message).Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Subscribe subscribes to a channel and returns a channel that receives messages
+func (s *RedisStore) Subscribe(channel string) (<-chan *redis.Message, error) {
+	pubsub := s.Client.Subscribe(channel)
+	_, err := pubsub.Receive()
+	if err != nil {
+		return nil, err
+	}
+
+	return pubsub.Channel(), nil
 }
