60f430559530212ee04ce29a903f437568c9276b
George Baronheid <baronheid.george@gmail.com>
2024-01-27 16:11:58 -0300

    api client pattern

---
+++ pkg/api/client.go
@@ -0,0 +1,92 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	kitlog "github.com/go-kit/log"
+	"net/http"
+)
+
+type Client struct {
+	baseURL    string
+	httpClient *http.Client
+	logger     kitlog.Logger
+}
+
+func NewClient(baseURL string, httpClient *http.Client, logger kitlog.Logger) *Client {
+	return &Client{
+		baseURL:    baseURL,
+		httpClient: httpClient,
+		logger:     logger,
+	}
+}
+
+func (c *Client) CreatePayment(request CreatePaymentRequest) (CreatePaymentResponse, error) {
+	url := fmt.Sprintf("%s/payments", c.baseURL)
+
+	payload, err := json.Marshal(request)
+	if err != nil {
+		return CreatePaymentResponse{}, err
+	}
+
+	req, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(payload))
+	if err != nil {
+		return CreatePaymentResponse{}, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return CreatePaymentResponse{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return CreatePaymentResponse{}, errors.New("unexpected status code")
+	}
+
+	var responseBody CreatePaymentResponse
+	err = json.NewDecoder(resp.Body).Decode(&responseBody)
+	if err != nil {
+		return CreatePaymentResponse{}, err
+	}
+
+	return responseBody, nil
+}
+
+func (c *Client) GetPayment(request GetPaymentRequest) (GetPaymentResponse, error) {
+	url := fmt.Sprintf("%s/payments", c.baseURL)
+
+	payload, err := json.Marshal(request)
+	if err != nil {
+		return GetPaymentResponse{}, err
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, bytes.NewBuffer(payload))
+	if err != nil {
+		return GetPaymentResponse{}, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return GetPaymentResponse{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return GetPaymentResponse{}, errors.New("unexpected status code")
+	}
+
+	var responseBody GetPaymentResponse
+	err = json.NewDecoder(resp.Body).Decode(&responseBody)
+	if err != nil {
+		return GetPaymentResponse{}, err
+	}
+
+	return responseBody, nil
+}
---
+++ pkg/api/structs.go
@@ -0,0 +1,54 @@
+package api
+
+import (
+	"github.com/shopspring/decimal"
+	"time"
+)
+
+type Payment struct {
+	ID        uuid.UUID
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Price     decimal.Decimal
+	OrderID   uuid.UUID
+	Status    PaymentStatus
+}
+
+type PaymentStatus string
+
+const (
+	PaymentStatusPaid    PaymentStatus = "paid"
+	PaymentStatusPending PaymentStatus = "pending"
+	PaymentStatusFailed  PaymentStatus = "failed"
+	// TO-DO PaymentStatusClosed  PaymentStatus = "closed"
+)
+
+type CreatePaymentRequest struct {
+	Payment Payment `json:"payment"`
+}
+
+type CreatePaymentResponse struct {
+	PaymentID uuid.UUID     `json:"payment_id"`
+	Status    PaymentStatus `json:"status"`
+}
+
+type UpdatePaymentRequest struct {
+	PaymentID     uuid.UUID     `json:"payment_id"`
+	PaymentStatus PaymentStatus `json:"payment_status"`
+}
+
+type UpdatePaymentResponse struct {
+	PaymentID    uuid.UUID     `json:"payment_id"`
+	Status       PaymentStatus `json:"status"`
+	PaymentError string        `json:"payment_error,omitempty"`
+}
+
+type GetPaymentRequest struct {
+	PaymentID uuid.UUID `json:"payment_id"`
+}
+
+type GetPaymentResponse struct {
+	Payment      Payment       `json:"payment"`
+	Status       PaymentStatus `json:"status"`
+	PaymentError string        `json:"payment_error,omitempty"`
+}
